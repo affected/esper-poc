85e4e49290b2c4abd5c775dad6ebb553f7798a63
Mikael Gueck <gumi@iki.fi>
2015-04-10 22:23:54 +0300

    initial

---
+++ src/main/java/io/mikael/nioth2015/Application.java
@@ -0,0 +1,13 @@
+package io.mikael.nioth2015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String ... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
