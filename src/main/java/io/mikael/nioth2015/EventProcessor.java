9aa345df488575c920ba732471079b2baafba72b
Mikael Gueck <gumi@iki.fi>
2015-04-11 14:54:02 +0300

    Custom timestamps.

--- src/main/java/io/mikael/nioth2015/EventProcessor.java
+++ src/main/java/io/mikael/nioth2015/EventProcessor.java
@@ -1,28 +1,39 @@
 package io.mikael.nioth2015;
 
+import com.espertech.esper.client.Configuration;
 import com.espertech.esper.client.EPServiceProvider;
 import com.espertech.esper.client.EPServiceProviderManager;
+import com.espertech.esper.client.time.CurrentTimeEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mikael.nioth2015.model.TemperatureEvent;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Component
 public class EventProcessor {
 
-    public final EPServiceProvider epService = EPServiceProviderManager.getDefaultProvider();
+    private EPServiceProvider esperProvider;
+
+    @PostConstruct
+    public void postConstruct() {
+        final Configuration config = new Configuration();
+        config.getEngineDefaults().getThreading().setInternalTimerEnabled(false);
+        esperProvider = EPServiceProviderManager.getDefaultProvider(config);
+    }
 
     public void submitMessage(final String jsonPayload) {
         try {
-            epService.getEPRuntime().sendEvent(new ObjectMapper().readValue(jsonPayload, TemperatureEvent.class));
+            submitEvent(new ObjectMapper().readValue(jsonPayload, TemperatureEvent.class));
         } catch (final IOException e) {
             throw new RuntimeException("temperature event json deserialization failed", e);
         }
     }
 
     public void submitEvent(final TemperatureEvent event) {
-        epService.getEPRuntime().sendEvent(event);
+        esperProvider.getEPRuntime().sendEvent(new CurrentTimeEvent(event.timeCreated.toInstant().toEpochMilli()));
+        esperProvider.getEPRuntime().sendEvent(event);
     }
 
 }
